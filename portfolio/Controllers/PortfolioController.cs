30aeea14530d95f0962bac23fc93c18114495d73
SaeedShariati <doloresdotnet@gmail.com>
2021-12-12 15:20:18 +0330

    added resume

--- portfolio/Controllers/PortfolioController.cs
+++ portfolio/Controllers/PortfolioController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,12 @@ namespace portfolio.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Resume()
+        {
+            string filepath = @"\files\Resume.pdf";
+            return File(filepath, "application/pdf", Path.GetFileName(filepath));
         }
     }
 }
---
+++ portfolio/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace portfolio.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string[] Tags { get; set; }
+        public string Text { get; set; }
+        public string Header { get; set; }
+        public string Author { get; set; }
+        public string[] Comments { get; set; }
+
+    }
+}
